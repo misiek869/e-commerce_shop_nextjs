4be51caae44a2ae37dc6860a6281079b0889fb3f
Michal Gawik <gawlik.zator@o2.pl>
2025-03-14 11:12:19 +0100

    header complete

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -10,17 +10,31 @@ const Header = () => {
 			<div className='wrapper flex-between'>
 				<div className='flex-start'>
 					<Link href={'/'}>
-						<Image
-							src='./images/Logo.svg'
-							height={48}
-							width={48}
-							alt='logo'
-							priority={true}
-						/>
-						<span className='hidden lg:visible font-bold text-2xl ml-3 '>
-							Super Shop
-						</span>
+						<div className='flex items-center'>
+							<Image
+								src='./images/Logo.svg'
+								height={48}
+								width={48}
+								alt='logo'
+								priority={true}
+							/>
+							<span className='hidden lg:block font-bold text-2xl ml-3'>
+								Detailing Shop
+							</span>
+						</div>
 					</Link>
+				</div>
+				<div className='space-x-2'>
+					<Button asChild variant={'ghost'}>
+						<Link href={'/cart'}>
+							<CiShoppingCart /> Cart
+						</Link>
+					</Button>
+					<Button asChild variant={'ghost'}>
+						<Link href={'/sign-in'}>
+							<CiUser /> Sign In
+						</Link>
+					</Button>
 				</div>
 			</div>
 		</header>
