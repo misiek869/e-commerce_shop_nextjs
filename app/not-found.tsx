4f69270a989f8ece79befb2dbd7d59dc8b33dea2
Michal Gawik <gawlik.zator@o2.pl>
2025-03-14 12:53:31 +0100

    not found page complete

---
+++ app/not-found.tsx
@@ -0,0 +1,31 @@
+'use client'
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+
+const NotFoundPage = () => {
+	return (
+		<div className='flex flex-col min-h-screen justify-center items-center'>
+			<Image
+				src={'/images/Logo.svg'}
+				width={120}
+				height={120}
+				alt='Logo'
+				priority={true}
+			/>
+			<div className='text-center mt-12'>
+				<h1 className='text-3xl font-bold mb-4'>Not Found</h1>
+				<p className='text-destructive'>We can't find requested page </p>
+				<Button
+					variant={'outline'}
+					className='mt-4 ml-2'
+					onClick={() => {
+						window.location.href = '/'
+					}}>
+					Back Home
+				</Button>
+			</div>
+		</div>
+	)
+}
+
+export default NotFoundPage
