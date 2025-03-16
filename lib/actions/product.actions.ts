00855c5889ece657672379aec68c4e586c031db1
Michal Gawik <gawlik.zator@o2.pl>
2025-03-16 18:03:09 +0100

    Sets up WebSocket connections

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,12 +1,10 @@
 'use server'
 
-import { PrismaClient } from '@prisma/client'
+import { prisma } from '@/db/prisma'
 import { convertToJsPlainObject } from '../utils'
 import { LATEST_PRODUCTS_LIMIT } from '../constants'
 
 export async function getLatestProducts() {
-	const prisma = new PrismaClient()
-
 	const data = await prisma.product.findMany({
 		take: LATEST_PRODUCTS_LIMIT,
 
