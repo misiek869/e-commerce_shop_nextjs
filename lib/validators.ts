2ccb2e3c81198adcbd0ced8f2e1fd0465943a0fa
Michal Gawik <gawlik.zator@o2.pl>
2025-03-18 11:29:50 +0100

    create signInSchema

--- lib/validators.ts
+++ lib/validators.ts
@@ -20,3 +20,8 @@ export const insertProductSchema = z.object({
 	banner: z.string().nullable(),
 	price: currency,
 })
+
+export const signInSchema = z.object({
+	email: z.string().email('Invalid email adress'),
+	password: z.string().min(6, 'Password must be at least 6 characters'),
+})
