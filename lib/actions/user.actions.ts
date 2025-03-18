a8723d6f282bcf44f605ee24cda192aafc186b91
Michal Gawik <gawlik.zator@o2.pl>
2025-03-18 11:39:09 +0100

    create singInWithCredentials and singOutUser functions

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,30 @@
+'use server'
+
+import { signInSchema } from '../validators'
+import { signIn, signOut } from '@/auth'
+import { isRedirectError } from 'next/dist/client/components/redirect-error'
+
+export async function singInWithCredentials(
+	prevState: unknown,
+	formData: FormData
+) {
+	try {
+		const user = signInSchema.parse({
+			email: formData.get('email'),
+			password: formData.get('password'),
+		})
+
+		await signIn('credentials', user)
+
+		return { success: true, message: 'Signed in successfully' }
+	} catch (error) {
+		if (isRedirectError(error)) {
+			throw error
+		}
+		return { success: false, message: 'Invalid email od password' }
+	}
+}
+
+export async function singOutUser() {
+	await signOut()
+}
