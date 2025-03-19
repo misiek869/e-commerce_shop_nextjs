07ea1a6e01becf4c7626473c4c79d62ae4b1004a
Michal Gawik <gawlik.zator@o2.pl>
2025-03-19 13:18:30 +0100

    create formatError to show user exact errors

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,6 +5,7 @@ import { signIn, signOut } from '@/auth'
 import { isRedirectError } from 'next/dist/client/components/redirect-error'
 import { hashSync } from 'bcrypt-ts-edge'
 import { prisma } from '@/db/prisma'
+import { formatError } from '../utils'
 
 export async function singInWithCredentials(
 	prevState: unknown,
@@ -62,6 +63,6 @@ export async function signUpUser(prev: unknown, formData: FormData) {
 		if (isRedirectError(error)) {
 			throw error
 		}
-		return { success: false, message: 'Registration failed' }
+		return { success: false, message: formatError(error) }
 	}
 }
