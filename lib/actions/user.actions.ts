c9904d2849399bfd4b5eb6cb8d8ab2e0905358cd
Michal Gawik <gawlik.zator@o2.pl>
2025-03-19 12:01:09 +0100

    create signUpUser function

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,8 +1,10 @@
 'use server'
 
-import { signInSchema } from '../validators'
+import { signInSchema, signUpSchema } from '../validators'
 import { signIn, signOut } from '@/auth'
 import { isRedirectError } from 'next/dist/client/components/redirect-error'
+import { hashSync } from 'bcrypt-ts-edge'
+import { prisma } from '@/db/prisma'
 
 export async function singInWithCredentials(
 	prevState: unknown,
@@ -27,4 +29,39 @@ export async function singInWithCredentials(
 
 export async function singOutUser() {
 	await signOut()
+}
+
+export async function signUpUser(prev: unknown, formData: FormData) {
+	try {
+		const user = signUpSchema.parse({
+			email: formData.get('email'),
+			name: formData.get('name'),
+			password: formData.get('password'),
+			confirmPassword: formData.get('confirmPassword'),
+		})
+
+		const plainPassword = user.password
+
+		user.password = hashSync(user.password, 10)
+
+		await prisma.user.create({
+			data: {
+				name: user.name,
+				email: user.email,
+				password: user.password,
+			},
+		})
+
+		await signIn('credentials', {
+			email: user.email,
+			password: plainPassword,
+		})
+
+		return { success: true, message: 'Successful registration!' }
+	} catch (error) {
+		if (isRedirectError(error)) {
+			throw error
+		}
+		return { success: false, message: 'Registration failed' }
+	}
 }
