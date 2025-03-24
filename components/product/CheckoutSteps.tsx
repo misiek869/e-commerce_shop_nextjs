9450e5f6b38ec9c140ad31208abebd48fa8cc7d3
Michal Gawik <gawlik.zator@o2.pl>
2025-03-24 13:02:28 +0100

    create component

---
+++ components/product/CheckoutSteps.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import { cn } from '@/lib/utils'
+
+const CheckoutSteps = ({ current = 0 }) => {
+	return (
+		<div className='flex-between flex-col space-x-2 space-y-2 mb-10 md:flex-row'>
+			{['User Login', 'Shipping Address', 'Payment Method', 'Place Order'].map(
+				(item, index) => {
+					return (
+						<React.Fragment key={index}>
+							<div
+								className={cn(
+									'p-2 w-56 rounded-full text-center text-sm',
+									index === current ? 'bg-secondary' : ''
+								)}>
+								{item}
+							</div>
+							{item !== 'Place Order' && (
+								<hr className='w-16 border-slate-600 mx-2'></hr>
+							)}
+						</React.Fragment>
+					)
+				}
+			)}
+		</div>
+	)
+}
+
+export default CheckoutSteps
