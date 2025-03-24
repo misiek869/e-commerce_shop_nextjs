f6f780669bdae18484b304bd3d947bfa83a65b8d
Michal Gawik <gawlik.zator@o2.pl>
2025-03-24 08:58:58 +0100

    create shippingAddressSchema and ShippingAddressType

--- lib/validators.ts
+++ lib/validators.ts
@@ -56,3 +56,13 @@ export const insertCartSchema = z.object({
 	sessionCartId: z.string().min(1, 'Session cart id is required'),
 	userId: z.string().optional().nullable(),
 })
+
+export const shippingAddressSchema = z.object({
+	fullName: z.string().min(3, 'Name must be at least 3 characters'),
+	streetAddress: z.string().min(3, 'Street must be at least 3 characters'),
+	city: z.string().min(3, 'City must be at least 3 characters'),
+	postalCode: z.string().min(3, 'Post code must be at least 3 characters'),
+	country: z.string().min(3, 'Country code must be at least 3 characters'),
+	latitude: z.number().optional(),
+	longitude: z.number().optional(),
+})
